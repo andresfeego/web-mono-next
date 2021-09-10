9638afc9a9633b2fc36382b8a0e37bb20a425820
Andres Manrique <andres.feego@gmail.com>
2021-09-09 23:08:06 -0500

    Mono administracion de marcas

---
+++ Components/AdminMarcas/AdminMarcas.js
@@ -0,0 +1,37 @@
+
+import styles from './AdminMarcas.module.scss'
+import { useState, useEffect } from 'react';
+import AgregarMarca from './AgregarMarca';
+
+
+
+const AdminMarcas = () => {
+
+    const [listaMarcas, setListaMarcas] = useState([])
+
+    const getMarcas = async () => {
+        const resMarcas = await fetch('http://localhost:3020/responseMono/marcas')
+        const marcasJson = await resMarcas.json()
+        setListaMarcas(marcasJson)
+    }
+
+    useEffect(() => {
+        getMarcas()
+    }, [])
+
+
+    return (
+        <div className="containerVentana">
+            <AgregarMarca getMarcas={getMarcas} />
+            <div className={styles.adminMarcas}>
+                {listaMarcas.length}
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+export default AdminMarcas
